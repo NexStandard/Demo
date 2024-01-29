698e05a607ad713529146784ecb6cea93757ca4b
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-29 01:19:36 +0100

    fix visibility

--- ClassLibrary1/Component.cs
+++ ClassLibrary1/Component.cs
@@ -1,4 +1,5 @@
-﻿using Stride.Engine;
+﻿using ClassLibrary2;
+using Stride.Engine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1;
-internal class Component : SyncScript
+public class Component : SyncScript, IComp1
 {
     public override void Update()
     {
